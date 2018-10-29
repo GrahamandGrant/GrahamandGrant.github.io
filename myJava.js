13b8d05244b3813cb3ff930bf37c7ec534182625
GrahamandGrant <44241475+GrahamandGrant@users.noreply.github.com>
2018-10-28 19:29:46 -0600

    Add files via upload

--- myJava.js
+++ myJava.js
@@ -15,6 +15,10 @@ function getBig(){
         //$(".mybox2").fadeIn(500);
         $(".box2").show();
     }, 2000)
+    var l = document.getElementById("image");
+    var m = document.getElementById("text");
+    l.style.display = "none";
+    m.style.display = "none";
 }
 
 function myFunction(id) {
@@ -25,37 +29,85 @@ function myFunction(id) {
     var b = document.getElementById("Buy");
     var c = document.getElementById("Test");
     var d = document.getElementById("Cont");
+    var e = document.getElementById("sale");
+    var l = document.getElementById("image");
+    var m = document.getElementById("text");
+    l.style.display = "none";
+    m.style.display = "none";
     if (id === "Home"){
     	a.style.display = "none";
     	b.style.display = "none";
     	c.style.display = "none";
     	d.style.display = "none";
+        e.style.display = "none";
+        l.style.display = "none";
+        m.style.display = "none";
     }
     if (id === "Buy"){
     	a.style.display = "none";
     	z.style.display = "none";
     	c.style.display = "none";
     	d.style.display = "none";
+        e.style.display = "none";
+        l.style.display = "none";
+        m.style.display = "none";
     }
     if (id === "How"){
     	b.style.display = "none";
     	z.style.display = "none";
     	c.style.display = "none";
     	d.style.display = "none";
+        e.style.display = "none";
+        l.style.display = "none";
+        m.style.display = "none";
     }
     if (id === "Test"){
     	b.style.display = "none";
     	z.style.display = "none";
     	a.style.display = "none";
     	d.style.display = "none";
+        e.style.display = "none";
+        l.style.display = "none";
+        m.style.display = "none";
     }
     if (id === "Cont"){
     	b.style.display = "none";
     	z.style.display = "none";
     	c.style.display = "none";
     	a.style.display = "none";
+        e.style.display = "none";
+        l.style.display = "none";
+        m.style.display = "none";
     }
     if (x.style.display === "none") {
         x.style.display = "block";
+    }
+}
+function myFunction2(id) {
+    var x = ''
+    var x = document.getElementById(id);
+    var a = document.getElementById("Buy");
+    var b = document.getElementById("sale")
+    var c = document.getElementById("Test");
+    var d = document.getElementById("Cont");
+    a.style.display = "none";
+
+    if (id === "Home"){
+        b.style.display = "none";
+    }
+    if (id === "Buy"){
+        b.style.display = "none";
+    }
+    if (id === "How"){
+        b.style.display = "none";
+    }
+    if (id === "Test"){
+        b.style.display = "none";
+    }
+    if (id === "Cont"){
+        b.style.display = "none";
+    }
+        if (x.style.display === "none") {
+    x.style.display = "block";
     }
 }
